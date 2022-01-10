3202f668d2dfe3d78cb9f0f43222112734723966
Mr.n0b0dy <archi3run@gmail.com>
2022-01-10 19:28:41 +0300

    add folder (api)

---
+++ src/Api/api.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const instance = axios.create({
+   baseURL: 'https://neko-back.herokuapp.com/2.0',
+   withCredentials: true
+})
+
+
+const testApi = {
+   test() {
+      return instance.get('')
+   }
+}
