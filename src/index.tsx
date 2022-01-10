773ec2d40599b1c2f336ebcccfe35f21174780fb
Mr.n0b0dy <archi3run@gmail.com>
2022-01-10 15:17:12 +0300

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,20 @@ import './Styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
 
 ReactDOM.render(
-   <HashRouter>
-      <React.StrictMode>
-         <App/>
-      </React.StrictMode>
-   </HashRouter>,
+   <React.StrictMode>
+      <Provider store={store}>
+         <HashRouter>
+            <App/>
+         </HashRouter>
+      </Provider>
+   </React.StrictMode>,
    document.getElementById('root')
-);
+)
+;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
