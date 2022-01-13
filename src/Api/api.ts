b41535fbc38b6c6dce3c7e36e52b81d59d363212
KrStasya <anastasiya.krautsova@gmail.com>
2022-01-13 13:33:54 +0300

    add registrationPage

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -1,13 +1,22 @@
 import axios from "axios";
 
-const instance = axios.create({
-   baseURL: 'https://neko-back.herokuapp.com/2.0',
+/*const instance = axios.create({
+   baseURL: "https://neko-back.herokuapp.com/2.0/",
    withCredentials: true
+})*/
+
+const instance = axios.create({
+    baseURL: "http://localhost:7542/2.0/",
+    withCredentials: true
 })
 
+export const authApi = {
+    signUp(email: string, password: string) {
+        return instance.post<newUserApiType>('/auth/register', {email, password})
+    },
+}
 
-const testApi = {
-   test() {
-      return instance.get('')
-   }
+export type newUserApiType = {
+    addedUser: {}
+    error: string
 }
