c5bf9104051f1a8c51860c837993d635b3618620
Mr.G3ntl3man <archi3run@gmail.com>
2022-01-13 16:14:38 +0300

    change styles

--- src/Components/Pages/Registration.tsx
+++ src/Components/Pages/Registration.tsx
@@ -10,38 +10,38 @@ import styles from "../../Styles/Registration.module.scss";
 
 export const Registration = () => {
 
-    const registrateEmail = useSelector((store: AppRootStateT) => store.signUp.email);
-    const error = useSelector((store: AppRootStateT) => store.signUp.error)
-    const isOldUser = useSelector((store: AppRootStateT) => store.signUp.isOldUser)
+   const registrateEmail = useSelector((store: AppRootStateT) => store.signUp.email);
+   const error = useSelector((store: AppRootStateT) => store.signUp.error)
+   const isOldUser = useSelector((store: AppRootStateT) => store.signUp.isOldUser)
 
-    const [email, setEmail] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
-    const [password1, setPassword1] = useState<string>("")
+   const [email, setEmail] = useState<string>("")
+   const [password, setPassword] = useState<string>("")
+   const [password1, setPassword1] = useState<string>("")
 
-    const dispatch = useDispatch()
+   const dispatch = useDispatch()
 
 
-    const signUpHandler = useCallback((email: string, password: string) => {
-        if (password1 === password) {
-            dispatch(regNewUser(email, password))
-        } else {
-            dispatch(setErrorRegistration("Пароли должны совпадать"))
-        }
-    }, [dispatch, email, password1, password])
+   const signUpHandler = useCallback((email: string, password: string) => {
+      if (password1 === password) {
+         dispatch(regNewUser(email, password))
+      } else {
+         dispatch(setErrorRegistration("Пароли должны совпадать"))
+      }
+   }, [dispatch, email, password1, password])
 
-    if (isOldUser || registrateEmail === email) {
-        return <Navigate to={PATH.LOGIN}/>
-    }
+   if (isOldUser) {
+      return <Navigate to={PATH.LOGIN}/>
+   }
 
-    return (
-        <div>
-            Registration page
-            {error && <div style={{color: "red"}}>{error}</div>}
-            <Input value={email} onChangeText={setEmail} label={"email"}/>
-            <Input value={password} onChangeText={setPassword} label={"password"}/>
-            <Input value={password1} onChangeText={setPassword1} label={"password2"}/>
-            <Button onClick={() => signUpHandler(email, password)}>sign up</Button>
-        </div>
-    );
+   return (
+      <div className={styles.content}>
+         Registration page
+         {error && <div style={{color: "red"}}>{error}</div>}
+         <Input value={email} onChangeText={setEmail} label={"email"}/>
+         <Input value={password} onChangeText={setPassword} label={"password"}/>
+         <Input value={password1} onChangeText={setPassword1} label={"password2"}/>
+         <Button onClick={() => signUpHandler(email, password)}>sign up</Button>
+      </div>
+   );
 };
 
