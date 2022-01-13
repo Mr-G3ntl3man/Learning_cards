dcec5d8b5696bf5fd40672f9f2bb889d48badae8
Oleg655 <savinoleg95@gmail.com>
2022-01-13 22:45:00 +0300

    addedState

--- src/Components/Pages/RecoverPass.tsx
+++ src/Components/Pages/RecoverPass.tsx
@@ -1,17 +1,27 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { testApi } from '../../Api/api';
 
 export const RecoverPass = () => {
 
-const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
-   testApi.recoverPassForgot(e.currentTarget.value)
+   const [date, setDate] = useState('')
+
+const onSendDate = () => {
+   testApi.recoverPassForgot(date)
 }
+
+const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+   setDate(e.currentTarget.value)
+}
+
 
    return (
       <div>
          <form>
-            <input type="text" onChange={onChangeText}/>
-            <button>send</button>
+            <input type="text"
+             onChange={onChangeText}
+         
+             />
+            <button onClick={onSendDate}>send</button>
          </form>
       </div>
    );
