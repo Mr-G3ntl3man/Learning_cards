6521bdec69427a06d9e3b2ca42ebc5604db5e6ff
Mr.G3ntl3man <archi3run@gmail.com>
2022-01-13 14:41:35 +0300

    rename Api object to authApi

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -1,22 +1,28 @@
 import axios from "axios";
 
-/*const instance = axios.create({
+const instance = axios.create({
    baseURL: "https://neko-back.herokuapp.com/2.0/",
    withCredentials: true
-})*/
-
-const instance = axios.create({
-    baseURL: "http://localhost:7542/2.0/",
-    withCredentials: true
 })
 
+// const instance = axios.create({
+//     baseURL: "http://localhost:7542/2.0/",
+//     withCredentials: true
+// })
+
 export const authApi = {
-    signUp(email: string, password: string) {
-        return instance.post<newUserApiType>('/auth/register', {email, password})
-    },
+   signUp(email: string, password: string) {
+      return instance.post<newUserApiType>('/auth/register', {email, password})
+   },
+   recoverPassForgot(email: string) {
+      return instance.post('/auth/forgot', {email})
+   },
+   login(data: { email: string, password: string }) {
+      return instance.post('auth/login', data)
+   }
 }
 
 export type newUserApiType = {
-    addedUser: {}
-    error: string
+   addedUser: {}
+   error: string
 }
