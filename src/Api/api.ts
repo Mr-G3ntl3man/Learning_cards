4787e297ac651ed245e3e93c57a7054fafd6e150
Mr.G3ntl3man <archi3run@gmail.com>
2022-01-13 13:46:41 +0300

    Rename api object to (Neko API), add method (login)

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-   baseURL: 'https://neko-back.herokuapp.com/2.0',
+   baseURL: 'https://neko-back.herokuapp.com/2.0/',
    withCredentials: true
 })
 
 
-const testApi = {
-   test() {
-      return instance.get('')
+export const nekoApi = {
+   login(data: { email: string, password: string }) {
+      return instance.post('auth/login', data)
    }
 }
