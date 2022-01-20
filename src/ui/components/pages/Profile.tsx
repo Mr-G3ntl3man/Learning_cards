66302f24e245b9e4f2ae5ab5d250444cd70e2814
Mr.G3ntl3man <archi3run@gmail.com>
2022-01-20 20:45:42 +0300

    added basic authorization checks

--- src/ui/components/pages/Profile.tsx
+++ src/ui/components/pages/Profile.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {authStatuses, fetchMe} from "../../../bll/auth-reducer";
+import {useDispatch} from "react-redux";
+import {Navigate} from "react-router-dom";
+import {PATH} from "../../router/Routes";
+import {useAppSelector} from "../../../bll/store";
 
 export const Profile = () => {
+   const dispatch = useDispatch()
+   const authStatus = useAppSelector<authStatuses>(state => state.auth.authStatus)
+
+   useEffect(() => {
+      dispatch(fetchMe())
+   }, [])
+
+   if (authStatus === authStatuses.LOGIN) return <Navigate to={PATH.LOGIN}/>
+
    return (
       <div>
          Profile page
