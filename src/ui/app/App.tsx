9fac8a7a14a28f7c7a8d86926e0a0034b99545d9
Mr.G3ntl3man <archi3run@gmail.com>
2022-01-20 20:48:32 +0300

    add Spinner for first loading

--- src/ui/app/App.tsx
+++ src/ui/app/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Router} from "../router/Routes";
 import {Tooltip} from "../components/common/Tooltip";
+import {useAppSelector} from '../../bll/store';
+import {authStatuses, firstFetchMe} from "../../bll/auth-reducer";
+import {Spinner} from "../components/common/Spinner";
+import {useDispatch} from "react-redux";
 
-const App = () => (
-   <main>
-      <Tooltip/>
-      <Router/>
-   </main>
-)
+const App = () => {
+   const authStatus = useAppSelector<authStatuses>(state => state.auth.authStatus)
+
+   const dispatch = useDispatch()
+
+   useEffect(() => {
+      dispatch(firstFetchMe())
+   }, [])
+
+   if (authStatus === authStatuses.LOADING) return <Spinner center/>
+
+   return (
+      <main>
+         <Tooltip/>
+         <Router/>
+      </main>
+   )
+}
 
 
 export default App;
