91fdc1a66c3d2cb75f1e537deb0cf16737e118f2
Mr.G3ntl3man <archi3run@gmail.com>
2022-01-20 20:44:17 +0300

    add 'centre' prop for center positioning

--- src/ui/components/common/Spinner.tsx
+++ src/ui/components/common/Spinner.tsx
@@ -1,11 +1,12 @@
 import style from '../../styles/Spinner.module.scss'
 import {ReactSVG} from "react-svg";
 import spinner from '../../images/spinner.svg'
+import React from "react";
 
 
-export const Spinner = () => {
+export const Spinner: React.FC<{ center?: boolean }> = ({center}) => {
    return (
-      <div className={style.wrap}>
+      <div className={center ? `${style.wrap} ${style.center}` : style.wrap}>
          <div className={style.spinner}>
             <ReactSVG src={spinner}/>
          </div>
