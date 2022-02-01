a70ee08bca2a46fefa9ce8b8a3906b08d765c5fe
Mr.G3ntl3man <archi3run@gmail.com>
2022-02-01 21:06:13 +0300

    refactor: change props

--- src/ui/components/common/Spinner.tsx
+++ src/ui/components/common/Spinner.tsx
@@ -1,14 +1,15 @@
 import style from '../../styles/Spinner.module.scss'
 import {ReactSVG} from "react-svg";
 import spinner from '../../images/icons/spinner.svg'
+import spinnerAtom from '../../images/icons/spinerAtom.svg'
 import React from "react";
 
 
-export const Spinner: React.FC<{ center?: boolean }> = ({center}) => {
+export const Spinner: React.FC = () => {
    return (
-      <div className={center ? `${style.wrap} ${style.center}` : style.wrap}>
+      <div className={`${style.wrap} ${style.center}`}>
          <div className={style.spinner}>
-            <ReactSVG src={spinner}/>
+            <ReactSVG src={spinnerAtom}/>
          </div>
       </div>
    )
