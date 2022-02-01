94b2a4b676c8c9c1a1a211058cda0c90cd4538b8
Mr.G3ntl3man <archi3run@gmail.com>
2022-02-01 21:10:26 +0300

    feat: add new component

---
+++ src/ui/components/common/TablePack.tsx
@@ -0,0 +1,188 @@
+import React, {MouseEvent, useCallback, useEffect} from 'react';
+import styles from "../../styles/PacksList.module.scss";
+import {ReactSVG} from "react-svg";
+import arrow from "../../images/icons/sort.svg";
+import {useAppSelector} from "../../../bll/store";
+import {CardPacksT, RequestGetPacksT} from "../../../dal/pakcApi";
+import {debounce} from "../../../utils/debounce";
+import {
+   fetchPacks,
+   setPackPage,
+   setPackPageCount,
+   setPacksName,
+   setSortPacks,
+} from "../../../bll/packs-reducer";
+import {useDispatch} from "react-redux";
+import {Link} from "react-router-dom";
+import {ButtonDeletePack} from "../modal/ButtonDeletePack";
+import {ButtonEditPack} from "../modal/ButtonEditPack";
+import {Pagination} from "./Pagination";
+import {Select} from "./Select";
+import {Input} from "./Input";
+import {ButtonAddPack} from "../modal/ButtonAddPack";
+import {PATH} from "../../router/Routes";
+
+export const TablePack: React.FC<{ isOwner?: boolean, id?: string }> = ({isOwner, id}) => {
+   const dispatch = useDispatch()
+
+   const {
+      page,
+      pageCount,
+      packName,
+      sortPacks,
+      min,
+      max,
+      user_id
+   } = useAppSelector<RequestGetPacksT>(state => state.packs.requestPacks)
+
+   const loading = useAppSelector<boolean>(state => state.app.loading)
+   const packs = useAppSelector<CardPacksT[]>(state => state.packs.packs)
+   const maxPage = useAppSelector<number>(state => state.packs.uiOptions.maxPage)
+   const myId = useAppSelector<string | undefined>(state => state.auth.userData?._id)
+
+   const onChangeInputSearch = debounce((value: string) => dispatch(setPacksName(value)))
+   const onPageChange = useCallback((page: number) => dispatch(setPackPage(page)), [dispatch])
+   const onSelectChange = useCallback((pageCount: number) => dispatch(setPackPageCount(pageCount)), [dispatch])
+   const onSortPackCLik = (e: MouseEvent<HTMLSpanElement>) => e.target instanceof HTMLSpanElement && dispatch(setSortPacks(e.target.dataset.sort as string))
+
+   useEffect(() => {
+      if (!loading) dispatch(fetchPacks(id))
+   }, [packName, page, pageCount, sortPacks, min, max, user_id])
+
+   const packsList = packs.map((el, index) => (
+      <PacksItem
+         user_id={el.user_id}
+         key={el._id}
+         packId={el._id}
+         bgColor={index % 2 === 0 ? '#fff' : '#F8F7FD'}
+         name={el.name}
+         cards={el.cardsCount}
+         update={el.updated.split(':')[0].slice(0, -3)}
+         created={el.user_name}
+         isOwner={el.user_id === myId}/>))
+
+   return (
+      <>
+         <div className={styles.interaction}>
+            {
+               isOwner
+                  ? <>
+                     <Input
+                        value={packName}
+                        onChangeText={onChangeInputSearch}
+                        className={styles.inputSearch}
+                        variant={'outlined'}
+                        label={'Search...'}/>
+
+                     <ButtonAddPack user_id={id}/>
+                  </>
+                  : <Input
+                     value={packName}
+                     onChangeText={onChangeInputSearch}
+                     className={styles.inputSearch}
+                     variant={'outlined'}
+                     label={'Search...'}/>
+            }
+         </div>
+
+         <div className={styles.packs}>
+            <ul className={styles.packsHeader}>
+               <li className={styles.sort}>
+                  Name <span className={styles.arrowSort}> <ReactSVG src={arrow}/></span>
+
+                  <div className={styles.sortList}>
+                           <span onClick={onSortPackCLik} data-sort={'0name'}
+                                 className={sortPacks === '0name' ? `${styles.sortItem} ${styles.active}` : styles.sortItem}>In descending order</span>
+                     <span onClick={onSortPackCLik}
+                           data-sort={'1name'}
+                           className={sortPacks === '1name' ? `${styles.sortItem} ${styles.active}` : styles.sortItem}>In ascending order</span>
+                  </div>
+               </li>
+               <li className={styles.sort}>
+                  Cards <span className={styles.arrowSort}> <ReactSVG src={arrow}/></span>
+
+                  <div className={styles.sortList}>
+                           <span onClick={onSortPackCLik} data-sort={'0cardsCount'}
+                                 className={sortPacks === '0cardsCount' ? `${styles.sortItem} ${styles.active}` : styles.sortItem}>In descending order</span>
+                     <span onClick={onSortPackCLik}
+                           data-sort={'1cardsCount'}
+                           className={sortPacks === '1cardsCount' ? `${styles.sortItem} ${styles.active}` : styles.sortItem}>In ascending order</span>
+                  </div>
+               </li>
+               <li className={styles.sort}>
+                  Last Updated <span className={styles.arrowSort}> <ReactSVG src={arrow}/></span>
+
+                  <div className={styles.sortList}>
+                           <span onClick={onSortPackCLik} data-sort={'0updated'}
+                                 className={sortPacks === '0updated' ? `${styles.sortItem} ${styles.active}` : styles.sortItem}>In descending order</span>
+                     <span onClick={onSortPackCLik}
+                           data-sort={'1updated'}
+                           className={sortPacks === '1updated' ? `${styles.sortItem} ${styles.active}` : styles.sortItem}>In ascending order</span>
+                  </div>
+               </li>
+               <li>Created by</li>
+               <li>Actions</li>
+            </ul>
+
+            <div className={styles.packsItem}>
+               {packsList}
+               {!packs.length && <div className={styles.noPacks}>There are no packages here yet.</div>}
+            </div>
+         </div>
+
+         <div className={styles.packsFooter}>
+            <Pagination initialPage={page} onPageChange={onPageChange} pageCount={maxPage}/>
+
+            <div className={styles.showCard}>
+               Show
+               <Select onChange={onSelectChange} defaultValue={pageCount} items={[10, 20, 30]}/>
+               Cards per Page
+            </div>
+         </div>
+
+      </>
+   )
+}
+
+
+const PacksItem: React.FC<PacksItemT> = React.memo((
+   {
+      created, cards, update,
+      isOwner, name, bgColor, packId, user_id
+   }) => (
+   <ul style={{backgroundColor: bgColor}}>
+      <li><Link className={styles.packsName} to={`/packs-list/${name}/${packId}/${user_id}`}>{name}</Link></li>
+      <li>{cards}</li>
+      <li>{update}</li>
+      <li>
+         {isOwner
+            ? <Link to={PATH.PROFILE} className={styles.packsCreated}>{created}</Link>
+            : <Link to={`/profile/${created}/${user_id}`} className={styles.packsCreated}>{created}</Link>}</li>
+      <li className={styles.packsActionBtn}>
+         {
+            isOwner
+               ? <>
+                  <ButtonDeletePack user_id={user_id || ''} id={packId} packName={name}/>
+                  <ButtonEditPack user_id={user_id || ''} id={packId} name={name}/>
+                  {cards ? <Link to={`/learn-cards/${name}/${cards}/${packId}`} className={styles.btn}>Learn</Link> : ''}
+               </>
+               : <>
+                  {cards ? <Link to={`/learn-cards/${name}/${cards}/${packId}`} className={styles.btn}>Learn</Link> : ''}
+               </>
+         }
+      </li>
+   </ul>
+))
+
+type PacksItemT = {
+   user_id?: string
+   name: string
+   cards: number
+   update: string
+   created: string
+   bgColor: string
+   isOwner: boolean
+   packId: string
+}
+
+
--- src/ui/components/pages/UserProfile.tsx
+++ src/ui/components/pages/UserProfile.tsx
@@ -1,27 +1,23 @@
 import React, {useCallback} from 'react';
-import styles from '../../styles/Profile.module.scss'
-import {useDispatch} from "react-redux";
-import {setSelectedMinMaxRange} from "../../../bll/packs-reducer";
-import {useAppSelector} from "../../../bll/store";
-import {Spinner} from "../common/Spinner";
-import {debounce} from "../../../utils/debounce";
-import 'rc-slider/assets/index.css';
+import {useParams} from "react-router-dom";
+import styles from "../../styles/Profile.module.scss";
+import {ReactSVG} from "react-svg";
+import avatarDefault from "../../images/avatarDef.svg";
 import {InputRange} from "../common/InputRange";
 import {TablePack} from "../common/TablePack";
-import avatarDefault from '../../images/avatarDef.svg'
-import {ReactSVG} from "react-svg";
-import {Link} from "react-router-dom";
-import {PATH} from "../../router/Routes";
+import {useAppSelector} from "../../../bll/store";
+import {debounce} from "../../../utils/debounce";
+import {setSelectedMinMaxRange} from "../../../bll/packs-reducer";
+import {useDispatch} from "react-redux";
+import {Spinner} from "../common/Spinner";
 
-export const Profile = () => {
+export const UserProfile = () => {
+   const {user_name, user_id} = useParams()
    const dispatch = useDispatch()
 
    const loading = useAppSelector<boolean>(state => state.app.loading)
-   const myId = useAppSelector<string | undefined>(state => state.auth.userData?._id)
-   const myName = useAppSelector<string | undefined>(state => state.auth.userData?.name)
    const minRangeRes = useAppSelector<number>(state => state.packs.uiOptions.minRangeRes)
    const maxRangeRes = useAppSelector<number>(state => state.packs.uiOptions.maxRangeRes)
-   const avatar = useAppSelector<string | undefined>(state => state.auth.userData?.avatar)
    const onChangeInputRange = useCallback(debounce((value: number[]) => dispatch(setSelectedMinMaxRange(value[0], value[1]))), [dispatch])
 
    return (
@@ -32,15 +28,10 @@ export const Profile = () => {
             <div className={styles.leftColumn}>
                <div className={styles.profileDesc}>
                   <div className={styles.profileImage}>
-                     {avatar ? <img src={avatar} alt="avatar"/> : <ReactSVG src={avatarDefault} alt="avatar"/>}
+                     <ReactSVG src={avatarDefault} alt="avatar"/>
                   </div>
-                  <span className={styles.profileName}>{myName}</span>
+                  <span className={styles.profileName}>{user_name}</span>
                   <span className={styles.profileSpeciality}>Front-end developer</span>
-                  <button className={styles.editProfile}>
-                     <Link to={PATH.EDIT_PROFILE}>
-                        Edit profile
-                     </Link>
-                  </button>
                </div>
 
                <div className={styles.inputRange}>
@@ -51,11 +42,12 @@ export const Profile = () => {
             </div>
 
             <div className={styles.rightColumn}>
-               <h3>My packs list</h3>
+               <h3>Packs list: {user_name}</h3>
 
-               <TablePack id={myId} isOwner/>
+               <TablePack id={user_id}/>
             </div>
          </div>
       </>
-   )
-}
+   );
+};
+
